58dd0314af94dddcbfa0cc51a0334fd57c2fed1a
jda0043 <90729752+jda0043@users.noreply.github.com>
2023-11-13 15:55:56 -0600

    Add files via upload
    Updated files. .html, .css, and .js

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+function turnOnOverlay() {
+  var x = document.getElementById("overlay_content_2");
+  x.style.display = "inline";
+  x.style.position = "fixed";
+
+
+}
+
+function turnOFFOverlay() {
+  var x = document.getElementById("overlay_content_2");
+  x.style.display = "none";
+
+
+}
+
