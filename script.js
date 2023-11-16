bc2c6533a3365e30831c6c5545de45a0db0d991b
jda0043 <90729752+jda0043@users.noreply.github.com>
2023-11-16 15:03:04 -0600

    Add files via upload
    - Added Initial Login
    - Added programming activities (1: Building your app. 2: Using the random block.)
    - Some formatting changes
    - Separated html files for additional pages. 
    
    To-Do:
        - Finish the styling for the login
            - Note: Login icons/new page: Needed or better method
        - Finalize any changes needed for the programming activity
            - Add any additional units 
        - Add the final activity
        - Add additional login features if needed
        - Clean up code and comment

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 function turnOnOverlay() {
   var x = document.getElementById("overlay_content_2");
+  
   x.style.display = "inline";
   x.style.position = "fixed";
 
@@ -11,7 +12,26 @@ function turnOnOverlay() {
 function turnOFFOverlay() {
   var x = document.getElementById("overlay_content_2");
   x.style.display = "none";
+  
 
 
 }
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+function toggleAvatar() {
+  var x = document.getElementById("Avatar");
+  x.src = "/assets/avatar2.jpg";
+  document.getElementById('id01').style.display='none';
+}
+
+
 
